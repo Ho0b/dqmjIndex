96a879cc9cf73b29a8a277a2a9e162afbaa67016
Ho0b <netrahun47@gmail.com>
2025-06-21 17:03:16 -0400

    added arrow key for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import slime from './assets/slime.svg'
 import './App.css'
 import monsterJson from './json/monsters.json'
+
+let mList = document.getElementsByClassName("monsterDetailContainer")
 
 /*
 
@@ -27,25 +29,69 @@ function SearchBar(){
 }
 
 function MonsterDetail(){
+  let [slideIndex, setSlideIndex] = useState(0)
+  for(let i = slideIndex; i < mList.length; i++){
+    mList[i].style.display = "none"
+  }
+
+  function mListNext(){
+    if (slideIndex > mList.length){
+      slideIndex = 0
+    } else {
+      setSlideIndex(slideIndex + 1)
+    }
+    console.log(slideIndex)
+  }
+  function mListPrev(){
+    
+    if (slideIndex < 0){
+      slideIndex = mList.length
+    } else {
+      setSlideIndex(slideIndex - 1)
+    }
+    console.log(slideIndex)
+  }
 
   const result = monsterJson.monsters.map((mon, index) => (
 
-    <article key={index} id='monsterDetailContainer' >
+    <article key={index} className='monsterDetailContainer' >
       <img className='monsterSprite' src={mon.spriteUrl} alt='monster sprite'></img>
-      <aside id='detailGrid'>
-        <div><p>Name: {mon.name}</p> <p>ID: {mon.id}</p></div>
-        <div><p>Rank: {mon.rank}</p> <p>Family: {mon.family}</p> <p>Skillset: {mon.skillset}</p></div>
-        <div><p>Trait 1: {mon.trait1}</p> <p>Trait 2: {mon.trait2}</p> <p>Resistances: {mon.resistances}</p></div>
+      <aside className='detailGrid'>
+        <div>
+          <span><strong>{mon.name}</strong></span> 
+          <span>ID # <strong>{mon.id}</strong></span>
+        </div>
+        <div>
+          <span>Rank: <strong>{mon.rank}</strong></span> 
+          <span>Family: <strong>{mon.family}</strong></span> 
+          <span>Skillset: <strong>{mon.skillset}</strong></span>
+        </div>
+        <div>
+          <span>Traits: <strong>{mon.trait1}</strong> <strong>{(mon.trait2 != "") ? `& ${mon.trait2}` : ""}</strong></span>
+        </div>
+        <div>
+          <span>Resistances: <strong>{mon.resistances}</strong></span>
+        </div>
       </aside>
     </article>
-    
   ))
 
   return (
 
-    <section>
+    <section id='monsterCollection'>
       {result}
+
+      <div id='arrowContainer'>
+        <button className='ArrowCircle' type='button' onClick={mListPrev}>
+          <div id='leftArrow'></div>
+        </button>
+        <button className='ArrowCircle' type='button' onClick={mListNext}>
+          <div id='rightArrow'></div>
+        </button>
+      </div>
     </section>
+
+    
     
   )
 }
@@ -65,17 +111,18 @@ function handleSearch(){
 }
 
 
-
-
 function App() {
 
   return (
     <main>
-      <HeaderCom/>
+      <div id='topBar'>
+        <HeaderCom/>
 
-      <SearchBar/>
-
+        <SearchBar/>
+      </div>
+        
       <MonsterDetail/>
+
     </main>
   )
 }
